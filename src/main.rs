57417851b250a6388d56e7e2d5548ebcd352a959
Zenk41 <ardhidhani@gmail.com>
2024-03-30 17:40:48 +0700

    initial project

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use leptos::*;
+use rust_task_manager::App;
+
+fn main() {
+    // set up logging
+    _ = console_log::init_with_level(log::Level::Debug);
+    console_error_panic_hook::set_once();
+
+    mount_to_body(|| {
+        view! {
+            <App />
+        }
+    })
+}
